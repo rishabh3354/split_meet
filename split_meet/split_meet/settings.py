f0b1b39760b32950c97eef2ab7e9a2518529a215
anil <anil01081997@gmail.com>
2020-12-11 18:06:42 +0530

    setting check

--- split_meet/split_meet/settings.py
+++ split_meet/split_meet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'split_meet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'anildb',
+        'USER': 'postgres',
+        'PASSWORD': 'our_server@***',
+        'HOST': '107.174.35.243',
+        'PORT': '5432',
     }
 }
 
